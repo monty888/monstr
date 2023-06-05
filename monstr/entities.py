cc8a66029db8d2ae275fb9b61d335f429ee175f4
monty <Monty888@protonmail.com>
2023-06-05 16:51:12 +0100

    finished nip19 entities decoding and added encoding

--- monstr/entities.py
+++ monstr/entities.py
@@ -1,6 +1,13 @@
+"""
+    support for bech32 encoded entities
+    https://github.com/nostr-protocol/nips/blob/master/19.md
+"""
+
 import bech32
 from monstr.encrypt import Keys
 
+class UnknownEntity(Exception):
+    pass
 
 # this code is ripped and edited from https://github.com/ustropo/uttlv
 def parse_array(data, tag_map: dict = None) -> bool:
@@ -85,56 +92,221 @@ class Entities:
         Entities.bytes_to_hex(data)
 
     @staticmethod
-    def bytes_to_hex(data):
+    def bytes_to_hex(data: bytes):
         return ''.join([hex(i).replace('0x', '').rjust(2, '0') for i in data])
 
     @staticmethod
-    def encode(self):
-        pass
+    def bytes_to_str(data: bytes):
+        return data.decode('utf8')
+
+    @staticmethod
+    def bytes_to_int(data: bytes):
+        return int.from_bytes(data, byteorder='big')
+
+    @staticmethod
+    def make_int_arr(key, encode_str: str, format='hex'):
+        if format == 'hex':
+            ret = [int(encode_str[i:i + 2], 16) for i in range(0, len(encode_str), 2)]
+        else:
+            ret = [ord(e) for e in encode_str]
+
+        ret = [key, len(ret)] + ret
+        return ret
+
+    # @staticmethod
+    # def encode_field(key, prefix, data, format='hex'):
+    #     if format == 'hex':
+    #         as_int = [int(data[i:i + 2], 16) for i in range(0, len(data), 2)]
+    #     else:
+    #         as_int = data.encode('utf8')
+    #
+    #     as_int = [key, len(as_int)] + as_int
+    #     data = bech32.convertbits(as_int, 8, 5)
+    #     return bech32.bech32_encode(prefix, data)
+
+    @staticmethod
+    def encode(name: str, data: dict):
+        if name in ('npub', 'nsec', 'note'):
+            ret = Keys.hex_to_bech32(data, prefix=name)
+        else:
+            if name == 'nprofile':
+                int_arr = Entities.make_int_arr(0, data['pubkey'])
+            elif name == 'nevent':
+                int_arr = Entities.make_int_arr(0, data['event_id'])
+            elif name == 'nrelay':
+                int_arr = Entities.make_int_arr(0, data['relay'], format='utf8')
+            elif name == 'naddr':
+                int_arr = Entities.make_int_arr(0, data['id'], format='utf8')
+            else:
+                raise UnknownEntity(name)
+
+            if 'relay' in data and name in ('nprofile', 'nevent', 'naddr'):
+                relays = data['relay']
+                if isinstance(relays, str):
+                    relays = [relays]
+
+                for c_relay in relays:
+                    int_arr += Entities.make_int_arr(1, c_relay, format='utf8')
+
+            if 'author' in data and name in ('naddr', 'nevent'):
+                int_arr += Entities.make_int_arr(2, data['author'])
+
+            if 'kind' in data and name in ('naddr', 'nevent'):
+
+                int_b = [int(i) for i in int.to_bytes(data['kind'], byteorder='big', length=4)]
+
+                int_arr += [3, 4] + int_b
+
+            # finally output as bech32
+            data = bech32.convertbits(int_arr, 8, 5)
+            ret = bech32.bech32_encode(name, data)
+
+        return ret
+
 
     @staticmethod
     def decode(value: str):
-        ret = {}
-        if value.startswith('npub'):
-            ret['pub_key'] = Keys.get_key(value).public_key_hex()
-        elif value.startswith('nsec'):
-            k = Keys.get_key(value)
-            ret['pub_key'] = k.public_key_hex()
-            ret['priv_key'] = k.private_key_hex()
-        elif value.startswith('note'):
-            ret['event_id'] = Keys.bech32_to_hex(value)
+        """
+            npub, nsec, note are returned as str
+            everything else returned as dict
+            for npub, nsec and event note can just as easily use Keys.bech32 directly anyway
+        """
+        ret = None
+        if value.startswith('npub') or value.startswith('nsec') or value.startswith('note'):
+            ret = Keys.bech32_to_hex(value)
         elif value.startswith('nprofile'):
-
 
             as_int = bech32.bech32_decode(value)
             data = bech32.convertbits(as_int[1], 5, 8)
-            print(parse_array(data, {
+            ret = parse_array(data, {
                 0x00: {
                     'name': 'pubkey',
                     'format': Entities.bytes_to_hex
                 },
                 0x01: {
-                    'name': 'relay'
+                    'name': 'relay',
+                    'format': Entities.bytes_to_str
                 }
-            }))
+            })
 
+        elif value.startswith('nevent'):
+            as_int = bech32.bech32_decode(value)
+            data = bech32.convertbits(as_int[1], 5, 8)
+            ret = parse_array(data, {
+                0x00: {
+                    'name': 'event_id',
+                    'format': Entities.bytes_to_hex
+                },
+                0x01: {
+                    'name': 'relay',
+                    'format': Entities.bytes_to_str
+                },
+                0x2: {
+                    'name': 'author',
+                    'format': Entities.bytes_to_hex
+                },
+                0x3: {
+                    'name': 'kind',
+                    'format': Entities.bytes_to_int
+                }
+            })
+
+        elif value.startswith('nrelay'):
+            as_int = bech32.bech32_decode(value)
+            data = bech32.convertbits(as_int[1], 5, 8)
+            ret = parse_array(data, {
+                0x00: {
+                    'name': 'relay',
+                    'format': Entities.bytes_to_str
+                }
+            })
+
+        elif value.startswith('naddr'):
+            as_int = bech32.bech32_decode(value)
+            data = bech32.convertbits(as_int[1], 5, 8)
+            ret = parse_array(data, {
+                0x00: {
+                    'name': 'id',
+                    'format': Entities.bytes_to_str
+                },
+                0x2: {
+                    'name': 'author',
+                    'format': Entities.bytes_to_hex
+                },
+                0x3: {
+                    'name': 'kind',
+                    'format': Entities.bytes_to_int
+                }
+            })
 
         return ret
 
 
 def test():
     # just test the examples from https://github.com/nostr-protocol/nips/blob/master/19.md
-    assert Entities.decode('npub10elfcs4fr0l0r8af98jlmgdh9c8tcxjvz9qkw038js35mp4dma8qzvjptg')['pub_key'] == \
+
+    # npub encode and decode
+    assert Entities.decode('npub10elfcs4fr0l0r8af98jlmgdh9c8tcxjvz9qkw038js35mp4dma8qzvjptg') == \
            '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e'
+    assert Entities.encode('npub', '7e7e9c42a91bfef19fa929e5fda1b72e0ebc1a4c1141673e2794234d86addf4e') == \
+        'npub10elfcs4fr0l0r8af98jlmgdh9c8tcxjvz9qkw038js35mp4dma8qzvjptg'
 
-    assert Entities.decode('nsec1vl029mgpspedva04g90vltkh6fvh240zqtv9k0t9af8935ke9laqsnlfe5')['priv_key'] == \
+    # nsec encode and decode
+    assert Entities.decode('nsec1vl029mgpspedva04g90vltkh6fvh240zqtv9k0t9af8935ke9laqsnlfe5') == \
            '67dea2ed018072d675f5415ecfaed7d2597555e202d85b3d65ea4e58d2d92ffa'
+    assert Entities.encode('nsec', '67dea2ed018072d675f5415ecfaed7d2597555e202d85b3d65ea4e58d2d92ffa') == \
+        'nsec1vl029mgpspedva04g90vltkh6fvh240zqtv9k0t9af8935ke9laqsnlfe5'
 
-    print(Entities.decode('nprofile1qqsrhuxx8l9ex335q7he0f09aej04zpazpl0ne2cgukyawd24mayt8gpp4mhxue69uhhytnc9e3k7mgpz4mhxue69uhkg6nzv9ejuumpv34kytnrdaksjlyr9p'))
+    # nprofile encode and decode
+    nprofile_example = Entities.decode('nprofile1qqsrhuxx8l9ex335q7he0f09aej04zpazpl0ne2cgukyawd24mayt8gpp4mhxue69uhhytnc9e3k7mgpz4mhxue69uhkg6nzv9ejuumpv34kytnrdaksjlyr9p')
+
+    assert nprofile_example['pubkey'] == '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d'
+
+    assert 'wss://r.x.com' in nprofile_example['relay']
+    assert 'wss://djbas.sadkb.com' in nprofile_example['relay']
+
+    assert Entities.encode('nprofile', {
+        'pubkey': '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
+        'relay': ['wss://r.x.com','wss://djbas.sadkb.com']
+    }) == 'nprofile1qqsrhuxx8l9ex335q7he0f09aej04zpazpl0ne2cgukyawd24mayt8gpp4mhxue69uhhytnc9e3k7mgpz4mhxue69uhkg6nzv9ejuumpv34kytnrdaksjlyr9p'
+
+
+    # own examples to test
+
+    nevent_example = Entities.decode('nevent1qqs2309gmw657ys52967enxsrh2syu54tjss42xjc829f5ty9g8j4gspzpmhxue69uhkummnw3ezuamfdejsygzxljlrqe027xh8sy2xtyjwfzfrxcll8afxh4hh847psjckhkxwf5psgqqqqqqs32mxmc')
+    assert nevent_example['event_id'] == 'a8bca8dbb54f12145175ecccd01dd50272955ca10aa8d2c1d454d1642a0f2aa2'
+    assert nevent_example['author'] == '46fcbe3065eaf1ae7811465924e48923363ff3f526bd6f73d7c184b16bd8ce4d'
+    assert nevent_example['relay'] == 'wss://nostr.wine'
+    assert nevent_example['kind'] == 1
+
+    assert Entities.encode('nevent',{
+        'event_id': 'a8bca8dbb54f12145175ecccd01dd50272955ca10aa8d2c1d454d1642a0f2aa2',
+        'author': '46fcbe3065eaf1ae7811465924e48923363ff3f526bd6f73d7c184b16bd8ce4d',
+        'relay': 'wss://nostr.wine',
+        'kind': 1
+    }) == 'nevent1qqs2309gmw657ys52967enxsrh2syu54tjss42xjc829f5ty9g8j4gspzpmhxue69uhkummnw3ezuamfdejsygzxljlrqe027xh8sy2xtyjwfzfrxcll8afxh4hh847psjckhkxwf5psgqqqqqqs32mxmc'
+
+
+    naddr_example = Entities.decode('naddr1qq9rzd3h8y6nqwf5xyuqygzxljlrqe027xh8sy2xtyjwfzfrxcll8afxh4hh847psjckhkxwf5psgqqqw4rsty50fx')
+    # assert naddr_example['id'] == '1679509418' ---CHECK THIS FIRST...
+    assert naddr_example['author'] == '46fcbe3065eaf1ae7811465924e48923363ff3f526bd6f73d7c184b16bd8ce4d'
+    assert naddr_example['kind'] == 30023
+
+    assert Entities.encode('naddr', {
+        'id': '1679509418',
+        'author': '46fcbe3065eaf1ae7811465924e48923363ff3f526bd6f73d7c184b16bd8ce4d',
+        'kind': 30023
+    }) == 'naddr1qq9rzd3h8y6nqwf5xyuqygzxljlrqe027xh8sy2xtyjwfzfrxcll8afxh4hh847psjckhkxwf5psgqqqw4rsty50fx'
+
+
+    # note encode and decode
+    assert Entities.decode('note1g7vhs4fqs9rq4e0w8mpn0t8x907q8a4uauugrv75zhdl5knpddcsh2rcsv') == \
+        '479978552081460ae5ee3ec337ace62bfc03f6bcef3881b3d415dbfa5a616b71'
+    assert Entities.encode('note', '479978552081460ae5ee3ec337ace62bfc03f6bcef3881b3d415dbfa5a616b71') == \
+           'note1g7vhs4fqs9rq4e0w8mpn0t8x907q8a4uauugrv75zhdl5knpddcsh2rcsv'
+
 
     print('all OK')
-
-
 
 
 if __name__ == "__main__":
